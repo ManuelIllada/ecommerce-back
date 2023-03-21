2a436a36b0e27a06d942153474968f27b091f125
ManuelIllada <ignacio.illada@fing.edu.uy>
2023-03-21 16:12:36 -0300

    nuevo

--- routes/public.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const authenticationController = require("../controllers/authenticationController");
-
-router.post("/tokens", authenticationController.token);
-router.post("/users", authenticationController.store);
-
-module.exports = router;
